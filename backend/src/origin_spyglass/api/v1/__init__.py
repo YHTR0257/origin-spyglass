c62fb5be42fe7252e3f02b4353c556c3379e617f
Yuhi Teramoto <grandflight@outlook.com>
2026-04-14 17:18:17 +0900

    feat: Add docs API

--- backend/src/origin_spyglass/api/v1/__init__.py
+++ backend/src/origin_spyglass/api/v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .chat import router as chat_router
+from .docs import router as docs_router
 from .health import router as health_router
 from .models import router as models_router
 
@@ -8,5 +9,6 @@ v1_router = APIRouter()
 v1_router.include_router(health_router)
 v1_router.include_router(models_router)
 v1_router.include_router(chat_router)
+v1_router.include_router(docs_router)
 
 __all__ = ["v1_router"]
